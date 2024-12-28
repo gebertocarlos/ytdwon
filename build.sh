007bf42fb4b546f03ce931264abf3c12fe69d089
Okan Akdoğan <okanakdogan@Okans-MacBook-Air.local>
2024-12-28 12:15:18 +0300

    Update build configuration for Render

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
-# Exit on error
+# exit on error
 set -o errexit
 
-# Install system dependencies for yt-dlp
-apt-get update
-apt-get install -y ffmpeg python3-pip
-
 # Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Download and install ffmpeg binary
+mkdir -p bin
+curl -L https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz -o ffmpeg.tar.xz
+tar xf ffmpeg.tar.xz
+mv ffmpeg-master-latest-linux64-gpl/bin/ffmpeg bin/
+mv ffmpeg-master-latest-linux64-gpl/bin/ffprobe bin/
+rm -rf ffmpeg-master-latest-linux64-gpl ffmpeg.tar.xz
+
+# Add bin to PATH
+export PATH="$PATH:$PWD/bin"
